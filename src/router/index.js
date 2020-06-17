945987ab43a336e3941a3c767a882d7a8ad57e9f
lixuan <2657140631@qq.com>
2020-06-17 15:52:58 +0800

    个人中心
    包括交易和个人信息修改、密码修改

--- src/router/index.js
+++ src/router/index.js
@@ -10,10 +10,10 @@ import Certification from '../views/Certification.vue';
 import Recommend from '../views/Recommend.vue';
 import Case from '../views/Case.vue';
 import Product from '../views/Product.vue';
-import Transaction from '../views/Transaction.vue';
 import Register from '../views/Register.vue';
 import Release from '../views/Release.vue'
 import HelpDocument from '../views/HelpDocument.vue';
+import User from '../views/User.vue';
 Vue.use(VueRouter)
 
 const routes = [{
@@ -59,17 +59,18 @@ const routes = [{
 	name:'Product',
 	component:Product
 },{
-	path:'/transaction',
-	name:'Transaction',
-	component:Transaction
-},{
 	path:'/register',
 	name:'Register',
 	component:Register
 },{
+
 	path:'/helpdoc',
 	name:'HelpDocument',
 	component:HelpDocument
+},{
+	path:'/user',
+	name:'User',
+	component:User
 },
 
 ];
