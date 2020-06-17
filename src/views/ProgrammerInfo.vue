7f9179287f8933f372fd372ec512c387a8679165
tjustb17515416 <65804723+tjustb17515416@users.noreply.github.com>
2020-06-17 16:48:52 +0800

    接单人员界面

---
+++ src/views/ProgrammerInfo.vue
@@ -0,0 +1,286 @@
+<template>
+	<div style="height:100%;width:100%;margin-top:2em;">
+        <el-row :gutter="12" type="flex" class="row-bg el-row-two" justify="center">
+            <el-col :span="4">
+                <div class="grid-contenta ">
+                    <a>
+                        <img style="margin-top:20px;width: 104px;height:104px;object-fit: cover;border-radius: 50%;" src="https://inn.proginn.com/useralbum/21070/icon210701575528590.jpg!mediumicon"/>    
+                    </a>
+                </div>
+                <div class="grid-content ">
+                    <p style="color:#333" class="header">美男子</p>
+                    <p style="color:#999" class="introduction"><i class="el-icon-s-opportunity"></i>阿里巴巴股份有限公司高级前端工程师</p>
+                </div>
+                <div class="grid-contentb" style="margin-top:0.1rem;padding-top:20px;padding-left:30px;">
+                    <p class="work-price">
+                        <span class="price" style="margin-top:40px;
+                        font-size: 20px;color: #1e92f5;">2000</span>元/天
+                    </p>
+                    <p style="box-sizing: border-box;text-align:left;margin-top:5px;padding: 0 1rem;font-size: 16px;color: #cccccc;line-height: 20px;">
+                    可工作地点：上海(远程)</p>
+                    <p style="box-sizing: border-box;text-align:left;margin-top:5px;padding: 0 1rem;font-size: 16px;color: #cccccc;line-height: 20px;">可工作时间：工作日09:00-18:00、周末09:00-18:00</p>
+                    <p class="day"><a href="#" style="background-color:#37a1f9;border:1.5px solid #37a1f9;color:#ffffff;padding:10px 50px;padding-right:40px;padding-left:40px">按天预约</a></p>
+                    <p class="day1"><a href="#" style="background-color:#fff;border:1.5px solid #37a1f9;color:#37a1f9;padding:10px 50px;padding-right:40px;padding-left:40px">按月雇佣</a></p>
+                    <p class="day2" style="margin-bottom:2em;"><a href="#" style="background-color:#fff;border:1.5px solid #37a1f9;color:#37a1f9;padding:10px 50px;padding-right:40px;padding-left:40px">和TA聊聊</a></p>
+                </div>    
+            </el-col>
+            <el-col :span="16">
+                <div >
+                    <div class="grid-contentc" >
+                        <p style="color: rgba(0, 0, 0, .8);text-align: left;
+                        box-sizing: border-box;font-family: Lato, 'Helvetica Neue', Arial, Helvetica, sans-serif;
+                        line-height: 1.33em;margin: 0;padding: 7px 5px 7px 20px;font-size: 18px;font-weight: 400;
+                        border-bottom: 1px solid #eee;">个人简介 <i class="el-icon-question"></i></p>
+                    </div>
+                    <div class="grid-contentd">
+                        <p>目前个人时间充裕，可单人独立承担中小型项目开发，也可参与有源码管理的分布式开发项目，包括并不限于：web前后端开发，pc桌面端应用，小程序开发。</p>
+
+                        <p>技能：8年软件开发工作经验，前后端全栈工程师，精通微软技术体系下c#，.net，asp.net mvc，core，visual studio等技术和开发工具的使用，熟练使用原生html5+css3，javascript和vue，react框架，熟悉my sql，sql server数据库的使用和维护管理，熟悉redis ，rabbit mq 队列。</p>
+
+                        <p>能力：可以独立完成一个项目从功能规划设计、数据库，软件前后端开发，测试，到后期维护全生命周期建设，也可良好配合团队协作开发，代码风格清晰流畅，健壮性好，行事靠谱。</p>
+                    </div>
+                    <div class="grid-contente" >
+                        <p style="color: rgba(0, 0, 0, .8);text-align: left;
+                        box-sizing: border-box;font-family: Lato, 'Helvetica Neue', Arial, Helvetica, sans-serif;
+                        line-height: 1.33em;margin: 0;padding: 7px 5px 7px 20px;font-size: 18px;font-weight: 400;
+                        border-bottom: 1px solid #eee;"><i class="el-icon-notebook-1"></i> 工作经历 </p>
+                    </div>
+                    <div class="grid-contentf">
+                            
+                        <p class="title">
+                            <span>
+                                <i class="el-icon-star-on" style="padding:20px 20px;"></i>
+                                     2016-1 — 至今 河北新阳网络科技 技术主管</span>
+                            <span class="work-certify">已认证</span>
+                        </p>
+                        <p style="padding-left:20px;">
+                        主导多个项目从0到1的研发工作，商城网站，外贸商城，erp系统，cms系统，小程序，生活号，app等 阿里云，aws等网站的部署维护，分布式集群等
+                        </p>
+                        <p class="title">
+                            <span>
+                                <i class="el-icon-star-on" style="padding:20px 20px;"></i>
+                                     2012-6 - 2015-3北京希如软件科技有限公司</span>
+                            <span class="work-certify">已认证</span>
+                        </p>
+                        <p style="padding-left:20px;">
+                        商城，cms，erp系统等 企业流程软件 微信公众号 oa系统 手机端商城 社交分享 第三方登陆 微信支付宝招商登陆 微信小程序
+                        </p>                    
+                    </div>
+                    <div class="grid-contente" >
+                        <p style="color: rgba(0, 0, 0, .8);text-align: left;
+                        box-sizing: border-box;font-family: Lato, 'Helvetica Neue', Arial, Helvetica, sans-serif;
+                        line-height: 1.33em;margin: 0;padding: 7px 5px 7px 20px;font-size: 18px;font-weight: 400;
+                        border-bottom: 1px solid #eee;"><i class="el-icon-reading"></i> 教育经历 </p>
+                    </div>
+                    <div class="grid-contentf">     
+                        <p class="title">
+                            <span>
+                                <i class="el-icon-edit" style="padding:20px 20px;"></i>
+                                     2012-9 — 2016-6 北京科技大学天津学院 计算科学与技术 本科</span>
+                            <span class="work-certify">已认证</span>
+                        </p>
+                        <p style="padding-left:20px;">
+                        大学期间获得三次奖学金，英语四级，六级证书，网络部部长
+                        </p>                    
+                    </div>                
+                    <div class="grid-contente" >
+                        <p style="color: rgba(0, 0, 0, .8);text-align: left;
+                        box-sizing: border-box;font-family: Lato, 'Helvetica Neue', Arial, Helvetica, sans-serif;
+                        line-height: 1.33em;margin: 0;padding: 7px 5px 7px 20px;font-size: 18px;font-weight: 400;
+                        border-bottom: 1px solid #eee;"><i class="el-icon-phone"></i> 联系方式</p>
+                    </div>
+                    <div class="grid-contentf">            
+                        <p class="title">
+                                <p style="padding-left:20px;font-size:16px">  邮箱: 1323113@tvery.com </p>
+                                <p style="padding-left:20px;font-size:16px">  电话: 0579-28100031 11223456723</p>
+                                <p style="padding-left:20px;font-size:16px">  微信号: lllll1234566</p>   
+                        </p>                   
+                    </div>
+                </div>
+            </el-col>
+        </el-row>
+    </div>
+</template>
+<script>
+    export default{
+
+    mounted() {
+    		document
+      			.querySelector("body")
+      			.setAttribute("style", "background-color:#f8f8f8");
+  		},
+  		beforeDestroy() {
+    		document.querySelector("body").removeAttribute("style");
+		},
+
+}
+</script>
+
+<style scoped>
+.grid-contenta {
+    background-color: #ffffff;
+    border-radius: 4px;
+    min-height: 150px;
+    min-width: 100px;
+    line-height: 1.33;
+    color: rgba(0, 0, 0, .8);
+    font-family: tahoma, Helvetica, Arial, 'Hiragino Sans GB', "Microsoft Yahei", "\5FAE\8F6F\96C5\9ED1", "\5b8b\4f53", sans-serif;
+    font-size: 1rem;
+    box-sizing: border-box;
+    text-align: center;
+    position: relative;
+
+  }
+.grid-content {
+    background-color: #ffffff;
+    border-radius: 4px;
+    min-height: 150px;
+    min-width: 100px;
+    line-height: 1.33;
+
+    }
+.grid-contentb {
+    background-color: #fff;
+    border-radius: 4px;
+    min-height: 200px;
+    min-width: 100px;
+    line-height: 1.33;
+    padding-bottom: 10px;
+    }
+.grid-contentc {
+    background-color: #fff;
+    border-radius: 4px;
+    line-height: 2.33;
+    }
+.grid-contentd{
+    background-color: #fff;
+    border-radius: 4px;
+    line-height: 2.33;
+    font-family: tahoma, Helvetica, Arial, 'Hiragino Sans GB', "Microsoft Yahei", "\5FAE\8F6F\96C5\9ED1", "\5b8b\4f53", sans-serif;
+    text-align: left;
+    box-sizing: border-box;
+    overflow: hidden;
+    padding: 12px 20px 20px;
+    font-size: 14px;
+    color: #2b303c;
+    line-height: 26px;
+    }
+.grid-contente{
+    background-color: #fff;
+    border-radius: 4px;
+    line-height: 3px;
+    margin-top: 15px;
+    }
+.grid-contentf{
+    background-color: #ffffff;
+    border-radius: 4px;
+    min-height: 150px;
+    min-width: 100px;
+    line-height: 1.33;
+    background-color: #fff;
+    border-radius: 4px;
+    line-height: 2.33;
+    font-family: tahoma, Helvetica, Arial, 'Hiragino Sans GB', "Microsoft Yahei", "\5FAE\8F6F\96C5\9ED1", "\5b8b\4f53", sans-serif;
+    text-align: left;
+    box-sizing: border-box;
+    overflow: hidden;
+    padding: 12px 20px 20px;
+    font-size: 14px;
+    color: #2b303c;
+    line-height: 26px;
+    }
+.works{
+    line-height: 2.33;
+    font-family: tahoma, Helvetica, Arial, 'Hiragino Sans GB', "Microsoft Yahei", "\5FAE\8F6F\96C5\9ED1", "\5b8b\4f53", sans-serif;
+    text-align: inherit;
+    font-size: 1rem;
+    box-sizing: border-box;
+    margin: 0px;
+    padding: 10px 10px 10px 10px;
+    }
+
+.title{
+    font-family: tahoma, Helvetica, Arial, 'Hiragino Sans GB', "Microsoft Yahei";
+    line-height: 1.33;
+    text-align:left;
+    margin-bottom: 0.5px;
+    margin-top: 10px;
+    font-size: 16px;
+    color: #4ba8f9;
+    }
+.work-certify{
+
+    position: relative;
+    top: -1px;
+    left: 15px;
+    display: inline-block;
+    padding: 1px 3px;
+    background-color: #37a1f9;
+    color: #fff;
+    font-size: 12px;
+
+        
+    }
+.el-row-two {
+    margin-bottom: 80px;
+    margin-top: 5px;
+  }
+.header{
+    line-height: 2.33;
+    font-family: tahoma, Helvetica, Arial, 'Hiragino Sans GB', "Microsoft Yahei", "\5FAE\8F6F\96C5\9ED1", "\5b8b\4f53", sans-serif;
+    text-align: center;
+    font-size: 18px;
+    box-sizing: border-box;
+    text-decoration: none;
+    background: 0;
+    padding-top: 0.5px;
+    color: #333;
+  }
+.introduction{
+    font-family: tahoma, Helvetica, Arial, 'Hiragino Sans GB', "Microsoft Yahei", "\5FAE\8F6F\96C5\9ED1", "\5b8b\4f53", sans-serif;
+    box-sizing: border-box;
+    text-align: center;
+    margin-top:20px;
+    padding: 0 2rem;
+    font-size: 16px;
+    color: #cccccc;
+    line-height: 15px;
+  }
+.work-price{
+    font-family: tahoma, Helvetica, Arial, 'Hiragino Sans GB', "Microsoft Yahei", "\5FAE\8F6F\96C5\9ED1", "\5b8b\4f53", sans-serif;
+    line-height: 1.33;
+    padding: 0 0px 0 16px;
+    text-align: left;
+    color: #1e92f5;
+    font-size: 14px;
+    }
+.day{
+    min-height: .5em;
+    margin: 20px 1.5px 0px 3px;
+    padding: .48571em 1.025em;
+    line-height: 1;
+    font-size: 15px;
+    font-weight: 400;
+    color: #fff;
+    }
+    .day1{
+    min-height: .5em;
+    margin: 20px 1.5px 0px 3px;
+    padding: .48571em 1.025em;
+    line-height: 1;
+    font-size: 15px;
+    font-weight: 400;
+    color: #fff;
+    }
+    .day2{
+    min-height: .5em;
+    margin: 20px 1.5px 0px 3px;
+    padding: .48571em 1.025em;
+    line-height: 1;
+    font-size: 15px;
+    font-weight: 400;
+    color: #fff;
+    }
+
+
+</style>
